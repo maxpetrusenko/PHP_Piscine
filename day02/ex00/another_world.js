9335a0a50ec0068968e1be1e75904fd751e43e61
Maksym Petrusenko <mpetruse@e1z2r3p41.42.us.org>
2019-01-07 18:11:28 -0800

    d2 update

---
+++ day02/ex00/another_world.js
@@ -0,0 +1,3 @@
+let av = process.argv.slice(2,3).join().replace(/\s+/g, " ").trim()
+// let av = process.argv.slice(2,3).join().replace(/\t/g, " ").replace(/ +/g, " ").trim()
+console.log(av)
---
+++ day02/ex01/one_more_time.js
@@ -0,0 +1,5 @@
+let av = process.argv.slice(2).join()
+
+let time = (new Date(av).getTime()/1000)
+
+console.log(time)
